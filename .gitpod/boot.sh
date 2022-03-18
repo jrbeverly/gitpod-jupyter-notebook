f47e97a581358b82b005df95c95f62efac17c66e
Jonathan Beverly <jonathan@jrbeverly.me>
2022-03-18 02:38:47 +0000

    Baseline repository

---
+++ .gitpod/boot.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+pip3 install dash
+pip3 install dash-daq
+pip3 install jupyter pandas matplotlib
---
+++ README.md
@@ -0,0 +1,2 @@
+# gitpod-jupyter-notebook
+Validation of running notebooks in gitpod
